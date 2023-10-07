915a9a7024ef7ee8db61ed314e5192725d469049
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2023-10-07 12:31:28 +0900

    add plain/ssl arg to server

--- game/server.sh
+++ game/server.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 set -e
 
-python3 -m venv server-env
-source server-env/bin/activate
+python3 -m venv env-server
+source env-server/bin/activate
 python3 -m pip install -r game/requirements.txt
 
-export GAME_LOCAL="t"
+if [ "$1" = "plain" ]; then
+  export GAME_SSL="t"
+elif [ "$1" = "ssl" ]; then
+  export GAME_SSL="f"
+else
+  echo "Specify plain/ssl as first argument"
+  exit 1
+fi
 
 cd game
 python3 server.py --hostname=localhost 8888 team6 ../ca/team6
