031c82acf8ba1aeeb0516f754e81d91a4acf5da2
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2023-10-07 12:07:47 +0900

    add local/remote/standalone arg to client.sh

--- game/client.sh
+++ game/client.sh
@@ -10,10 +10,25 @@ maturin build --profile opt
 pip install target/wheels/*
 cd ../game
 
-
-certdir=$(dirname "$2")
-certdir="`cd "${certdir}";pwd`"
-certfile=$(basename "$2")
+if [ "$1" = "local" ]; then
+  hostname="localhost"
+  certpath="../ca/team6"
+  capath="../ca/CA-devel.crt"
+  standalone=""
+elif [ "$1" = "remote" ]; then
+  hostname="team6.hackceler8-2023.ctfcompetition.com"
+  certpath="$HOME/team6"
+  capath="$HOME/CA.crt"
+  standalone=""
+elif [ "$1" = "standalone" ]; then
+  hostname=""
+  certpath=""
+  capath=""
+  standalone="--standalone"
+else
+  echo "Specify local/remote/standalone as first argument"
+  exit 1
+fi
 
 cd game
-python3 client.py "$1" 8888 8889 "$certdir/$certfile"
+python3 client.py --ca="$capath" $standalone "$hostname" 8888 8889 "$certpath"
